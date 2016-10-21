3e375d20c93577d26aa63341b35226c618959990
brett.wingard@lancasterco.com <brett.wingard@lancasterco.com>
2016-10-21 13:32:40 -0400

    Initial Connit

--- Forms_Multipage_Generator/Form1.Designer.cs
+++ Forms_Multipage_Generator/Form1.Designer.cs
@@ -29,11 +29,382 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.cityStZipTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numOPgsTextBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.settingsPanel = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.leftRightPosNum = new System.Windows.Forms.NumericUpDown();
+            this.bottomTopPosNum = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textSpacingNum = new System.Windows.Forms.NumericUpDown();
+            this.label12 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.settingsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.leftRightPosNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bottomTopPosNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textSpacingNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "City, State, Zip";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(136, 48);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(179, 20);
+            this.titleTextBox.TabIndex = 4;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(136, 82);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(179, 20);
+            this.addressTextBox.TabIndex = 5;
+            // 
+            // cityStZipTextBox
+            // 
+            this.cityStZipTextBox.Location = new System.Drawing.Point(136, 115);
+            this.cityStZipTextBox.Name = "cityStZipTextBox";
+            this.cityStZipTextBox.Size = new System.Drawing.Size(179, 20);
+            this.cityStZipTextBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Number of pages:";
+            // 
+            // numOPgsTextBox
+            // 
+            this.numOPgsTextBox.Location = new System.Drawing.Point(136, 143);
+            this.numOPgsTextBox.MaxLength = 4;
+            this.numOPgsTextBox.Name = "numOPgsTextBox";
+            this.numOPgsTextBox.Size = new System.Drawing.Size(58, 20);
+            this.numOPgsTextBox.TabIndex = 8;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(367, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // settingsPanel
+            // 
+            this.settingsPanel.Controls.Add(this.label12);
+            this.settingsPanel.Controls.Add(this.textSpacingNum);
+            this.settingsPanel.Controls.Add(this.label11);
+            this.settingsPanel.Controls.Add(this.label10);
+            this.settingsPanel.Controls.Add(this.label9);
+            this.settingsPanel.Controls.Add(this.bottomTopPosNum);
+            this.settingsPanel.Controls.Add(this.leftRightPosNum);
+            this.settingsPanel.Controls.Add(this.saveBtn);
+            this.settingsPanel.Controls.Add(this.label6);
+            this.settingsPanel.Controls.Add(this.label8);
+            this.settingsPanel.Controls.Add(this.label7);
+            this.settingsPanel.Controls.Add(this.label5);
+            this.settingsPanel.Location = new System.Drawing.Point(11, 27);
+            this.settingsPanel.Name = "settingsPanel";
+            this.settingsPanel.Size = new System.Drawing.Size(344, 222);
+            this.settingsPanel.TabIndex = 10;
+            this.settingsPanel.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(198, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Pixels";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Label Left RIght Position";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(198, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Pixels";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(5, 62);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(134, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Label Top, Bottom Position";
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(8, 196);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 11;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // leftRightPosNum
+            // 
+            this.leftRightPosNum.Location = new System.Drawing.Point(140, 33);
+            this.leftRightPosNum.Maximum = new decimal(new int[] {
+            792,
+            0,
+            0,
+            0});
+            this.leftRightPosNum.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.leftRightPosNum.Name = "leftRightPosNum";
+            this.leftRightPosNum.Size = new System.Drawing.Size(56, 20);
+            this.leftRightPosNum.TabIndex = 12;
+            this.leftRightPosNum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // bottomTopPosNum
+            // 
+            this.bottomTopPosNum.Location = new System.Drawing.Point(140, 60);
+            this.bottomTopPosNum.Maximum = new decimal(new int[] {
+            612,
+            0,
+            0,
+            0});
+            this.bottomTopPosNum.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bottomTopPosNum.Name = "bottomTopPosNum";
+            this.bottomTopPosNum.Size = new System.Drawing.Size(56, 20);
+            this.bottomTopPosNum.TabIndex = 13;
+            this.bottomTopPosNum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(137, 4);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Settings";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(238, 50);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(89, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Default: 170, 650";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(37, 119);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(102, 13);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Text Group Spacing";
+            // 
+            // textSpacingNum
+            // 
+            this.textSpacingNum.Location = new System.Drawing.Point(140, 114);
+            this.textSpacingNum.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.textSpacingNum.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.textSpacingNum.Name = "textSpacingNum";
+            this.textSpacingNum.Size = new System.Drawing.Size(56, 20);
+            this.textSpacingNum.TabIndex = 17;
+            this.textSpacingNum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(198, 116);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(34, 13);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "Pixels";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 261);
+            this.Controls.Add(this.settingsPanel);
+            this.Controls.Add(this.numOPgsTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cityStZipTextBox);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.settingsPanel.ResumeLayout(false);
+            this.settingsPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.leftRightPosNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bottomTopPosNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textSpacingNum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox cityStZipTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox numOPgsTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Panel settingsPanel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.NumericUpDown leftRightPosNum;
+        private System.Windows.Forms.NumericUpDown bottomTopPosNum;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.NumericUpDown textSpacingNum;
+        private System.Windows.Forms.Label label11;
     }
 }
 
--- Forms_Multipage_Generator/Form1.cs
+++ Forms_Multipage_Generator/Form1.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +19,95 @@ namespace Forms_Multipage_Generator
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numberofpages = 0;
+            string title = titleTextBox.Text;
+            string address = addressTextBox.Text;
+            string citystzip = cityStZipTextBox.Text;
+            int leftrightpos = AppSettings.Default.Left_Right_LabelPosition;
+            int bottomtoppos = AppSettings.Default.Bottom_Top_LabelPosition;
+            int linespacing = AppSettings.Default.Text_Spacing;
+
+            if (numOPgsTextBox.Text != "")
+            {
+                try
+                {
+                    numberofpages = Convert.ToInt32(numOPgsTextBox.Text);
+                    PdfDocument pdf = new PdfDocument();
+                    pdf.Info.Title = title;
+
+                    //create however many pages we ask
+                    for (int i = 0; i < numberofpages; i++)
+                    {
+                        if (i == 0)
+                        {
+                            //first page get the address label
+                            PdfPage pdfPage = pdf.AddPage();
+                            pdfPage.Orientation = PdfSharp.PageOrientation.Landscape;
+                            XGraphics graph = XGraphics.FromPdfPage(pdfPage);
+                            XFont font = new XFont("helvetica narrow", 10); //XFontStyle.Bold
+                            //page width in pixels: 792
+                            //page height in pixels: 612
+                            //0,0 top left, 792,612 bottom right 
+                            graph.DrawString(title, font, XBrushes.Black, new Point(leftrightpos, bottomtoppos));
+                            graph.DrawString(address, font, XBrushes.Black, new Point(leftrightpos, bottomtoppos + linespacing));
+                            graph.DrawString(citystzip, font, XBrushes.Black, new Point(leftrightpos, bottomtoppos + linespacing + linespacing));
+                        }
+                        else
+                        {
+                            //create however many more "blank" pages
+                            PdfPage pdfPage = pdf.AddPage();
+                            pdfPage.Orientation = PdfSharp.PageOrientation.Landscape;
+                            XGraphics graph = XGraphics.FromPdfPage(pdfPage);
+                            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+                            graph.DrawString("", font, XBrushes.Black, new XRect(0, 0, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.Center);
+                        }
+                    }
+                    string pdfFilename = title;
+                    pdf.Save(pdfFilename);
+                    Process.Start(pdfFilename);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter the number of pages.");
+            }
+        }
+
+
+        #region toolbar
+        //toolbar items
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            settingsPanel.Show();
+            //load any settings that were saved
+            leftRightPosNum.Value = AppSettings.Default.Left_Right_LabelPosition;
+            bottomTopPosNum.Value = AppSettings.Default.Bottom_Top_LabelPosition;
+            textSpacingNum.Value = AppSettings.Default.Text_Spacing;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+        #region settings
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            AppSettings.Default.Left_Right_LabelPosition = Convert.ToInt32(leftRightPosNum.Value);
+            AppSettings.Default.Bottom_Top_LabelPosition = Convert.ToInt32(bottomTopPosNum.Value);
+            AppSettings.Default.Text_Spacing = Convert.ToInt32(textSpacingNum.Value);
+            settingsPanel.Hide();
+        }
+        #endregion
     }
 }
+
